513fa0928c7a20d3d5906ae4e6d6c2d2c6fbe058
40794434 <40794434@live.napier.ac.uk>
2025-11-06 11:59:10 +0630

    update pom.xml

--- src/test/java/org/example/AppIntegrationTest.java
+++ src/test/java/org/example/AppIntegrationTest.java
@@ -26,17 +26,5 @@ public class AppIntegrationTest {
         assertEquals(emp.last_name, "Garigliano");
     }
 
-    @Test
-    void testAddEmployee()
-    {
-        Employee emp = new Employee();
-        emp.emp_no = 500001;
-        emp.first_name = "Kevin";
-        emp.last_name = "Chalmers";
-        app.addEmployee(emp);
-        emp = app.getEmployee(500000);
-        assertEquals(emp.emp_no, 500000);
-        assertEquals(emp.first_name, "Kevin");
-        assertEquals(emp.last_name, "Chalmers");
-    }
+
 }
